0ab8599f146eb33af757ca2f29342af6894bdf6a
Menashe <mborukhov770@gmail.com>
2021-08-29 18:08:18 +0300

    addKeyVal complete

--- 10.4-rest-spread.js
+++ 10.4-rest-spread.js
@@ -18,19 +18,34 @@ const doubleAndReturnArgs = (arr, ...args) => [
 
 //
 // Slice and Dice
-// removeRandom
+/** remove a random element in the items array and return a new array without that item. */
 const removeRandom = (items) => {
-  let removedIdx = Math.floor(Math.random() * items.length);
-  console.log('Removed item:', items[removedIdx]);
-  return [...items.slice(0, removedIdx), ...items.slice(removedIdx + 1)];
+  let removedIndex = Math.floor(Math.random() * items.length);
+  console.log(`Removed item '${items[removedIndex]}' at index ${removedIndex}`);
+  return [...items.slice(0, removedIndex), ...items.slice(removedIndex + 1)];
 };
 
 //
-// extend
+/** Return a new array with every item in array1 and array2. */
 const extend = (array1, array2) => {
   return [...array1, ...array2];
 };
 
 //
-// addKeyVal
-const addKeyVal = (obj, key, val) => {};
+/** Return a new object with all the keys and values from obj and a new key/value pair */
+// let obj = {a:1, b:2}
+const addKeyVal = (obj, key, val) => {
+  let newObj = { ...obj };
+  obj[key] = val;
+  return newObj
+};
+
+//
+/** Return a new object with a key removed. */
+const removeKey = (obj, key) => {};
+
+/** Combine two objects and return a new object. */
+function combine(obj1, obj2) {}
+
+/** Return a new object with a modified key and value. */
+function update(obj, key, val) {}
